22c9e9219d7b5005209dd548e435ed648fbd72a1
coder_wannabe <50718882+chickoy050612@users.noreply.github.com>
2020-06-03 13:40:16 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,4 @@
+﻿SELECT Sales.SalesID, (CONVERT(varchar(50),Customers.LastName) + ', ' + CONVERT(varchar(50),Customers.FirstName)),  Products.ManufactCode, Products.Description, Sales.QtySold, Sales.SellingPrice
+FROM Customers INNER JOIN Sales ON Customers.CustomerID = Sales.CusID
+INNER JOIN Products ON Products.ProductID = Sales.ProductID
+ORDER BY 1;
